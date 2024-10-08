d2fefd1f9659959d3920391a6bd65504a5831778
LiGM-GH <lipkin.grisha@mail.ru>
2024-10-08 22:36:48 +0300

    ... did it correctly

--- src/pt2.rs
+++ src/pt2.rs
@@ -4,6 +4,7 @@ use plotters::{
     backend::SVGBackend,
     chart::ChartBuilder,
     drawing::IntoDrawingArea,
+    element::{Cross, EmptyElement},
     series::LineSeries,
     style::{BLUE, GREEN, RED, WHITE},
 };
@@ -21,41 +22,29 @@ pub fn main() -> Result<()> {
         .y_label_area_size(50)
         .build_cartesian_2d(0.0..1000.0, 0.0..50.0)?;
 
-    let mut w: Array1<f64> = Array1::from_elem(N, 0.0);
     let (x, y) = parse_data()?;
-    let mut res;
-    println!("Zeroes in y: {}", y.iter().any(|&y| y == 0));
 
-    for i in 0..100 {
-        for (x, y) in x.axis_iter(ndarray::Axis(1)).zip(y.clone()) {
-            let x = x.to_owned() / x[0];
-            res = i32::from(w.dot(&x) >= 0.0);
+    // for (i, y) in y.iter().enumerate() {
+    //     if *y == 1 {
+    //         println!("{}", i)
+    //     }
+    // }
 
-            if res == y {
-                continue;
-            }
-
-            if i == 80 {
-                println!(
-                    "w: {:>.2},\tx: {:>.2},\ty: {:>.4},\tres: {:>.4}",
-                    w, x, y, res
-                );
-            }
-
-            w = w + ETA * (y - res) as f64 * x.to_owned();
+    chart.draw_series(x.axis_iter(Axis(1)).enumerate().filter(|(i, _)| i % 100 == 0).map(|(i, elem)| {
+        match y[i] {
+            0 => EmptyElement::at((elem[0], elem[1])) + Cross::new((0, 0), 5, BLUE),
+            1 => EmptyElement::at((elem[0], elem[1])) + Cross::new((0, 0), 5, RED),
+            _ => panic!("DATA MALFORMED: YOU PUT SOME NON-ZERO AND NON-ONE VALUES AS FLAGS INTO THE CSV"),
         }
-    }
-    println!("\n{}\n", w);
+    }))?;
 
+    let mut w: Array1<f64> = Array1::from_elem(N, 0.0);
+    learn_inner(&mut w, &x, &y);
+    println!("\n{}\n", w);
     chart.draw_series(LineSeries::new(
-        (0..1000).map(|i| (i as f64, w[1] / w[0] * i as f64)),
+        (0..1000).map(|i| (i as f64, -w[0] / w[1] * i as f64)),
         &RED,
     ))?;
-
-    // chart.draw_series(LineSeries::new(
-    //     x.axis_iter(Axis(1)).map(|x| (x[0], x[1])),
-    //     &RED,
-    // ))?;
 
     chart.configure_mesh().draw()?;
     chart.configure_series_labels().draw()?;
@@ -101,8 +90,32 @@ pub fn parse_data() -> Result<(Array2<f64>, Array1<i32>)> {
     for i in 0..len {
         xcoords[(0, i)] = x.0[i];
         xcoords[(1, i)] = x.1[i];
-        ycoords[1] = y[i];
+        ycoords[i] = y[i];
     }
 
     Ok((xcoords, ycoords))
+}
+
+pub fn learn_inner(w: &mut Array1<f64>, x: &Array2<f64>, y: &Array1<i32>) {
+    let mut res;
+
+    for i in 0..100 {
+        for (x, y) in x.axis_iter(ndarray::Axis(1)).zip(y) {
+            let x = x.to_owned() / x[0];
+            res = i32::from(w.dot(&x) >= 0.0);
+
+            if res == *y {
+                continue;
+            }
+
+            if i == 80 {
+                println!(
+                    "w: {:>.2},\tx: {:>.2},\ty: {:>.4},\tres: {:>.4}",
+                    w, x, y, res
+                );
+            }
+
+            w.scaled_add(ETA * (y - res) as f64, &x);
+        }
+    }
 }
